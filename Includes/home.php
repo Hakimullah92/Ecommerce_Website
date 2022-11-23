3e89685e09de954d891b2d32edc5793cb5c1f213
Hakim Ullah <106232159+Hakimullah92@users.noreply.github.com>
2022-11-23 12:27:49 +0500

    Making responsive hero.php

--- Includes/home.php
+++ Includes/home.php
@@ -35,6 +35,121 @@
     <span class="visually-hidden">Next</span>
   </button>
 </div>
+
+
+<div class="container p-4">
+  <div class="row">
+    <h1 class="main-heading text-center mb-4"><span>NEW ARRIVAL</span></h1>
+  </div>
+  <div class="row justify-content-center mb-4">
+<div class="col-lg-3 col-md-4 col-sm-6">
+  <a href=""><img src="../images/pro1.jpg" alt="" class="img-fluid"></a>
+</div>
+<div class="col-lg-3 col-md-4 col-sm-6">
+  <a href=""><img src="../images/pro2.jpg" alt="" class="img-fluid"></a>
+</div>
+<div class="col-lg-3 col-md-4 col-sm-6">
+  <a href=""><img src="../images/pro3.jpg" alt="" class="img-fluid"></a>
+</div>
+<div class="col-lg-3 col-md-4 col-sm-6">
+  <a href=""><img src="../images/pro4.jpg" alt="" class="img-fluid"></a>
+</div>
+
+  </div>
+  <div class="row justify-content-center">
+  <div class="col-lg-3 col-md-4 col-sm-6">
+  <a href=""><img src="../images/pro5.jpg" alt="" class="img-fluid"></a>
+</div>
+<div class="col-lg-3 col-md-4 col-sm-6">
+  <a href=""><img src="../images/pro6.jpg" alt="" class="img-fluid"></a>
+</div>
+<div class="col-lg-3 col-md-4 col-sm-6">
+  <a href=""><img src="../images/pro7.jpg" alt="" class="img-fluid"></a>
+</div>
+<div class="col-lg-3 col-md-4 col-sm-6">
+  <a href=""><img src="../images/pro8.jpg" alt="" class="img-fluid"></a>
+</div>
+  </div>
+</div>
+
+
+<div class="container p-4">
+  <div class="row justify-content-center">
+    <h1 class="mains-heading text-uppercase text-center fw-bold fs-1 mb-4">looking for more?</h1>
+    <a href="" class=" arrival-link btn text-light fs-3 text-uppercase fw-light">view all new arrival dresses</a>
+    <h1 class="main-heading text-center mb-4 mt-5 text-uppercase"><span>winter collections</span></h1>
+  </div>
+
+  <div class="row justify-content-center">
+  <div class="col-lg-3 col-md-4 col-sm-6">
+  <a href=""><img src="../images/pro9.jpg" alt="" class="img-fluid"></a>
+</div>
+<div class="col-lg-3 col-md-4 col-sm-6">
+  <a href=""><img src="../images/pro10.jpg" alt="" class="img-fluid"></a>
+</div>
+<div class="col-lg-3 col-md-4 col-sm-6">
+  <a href=""><img src="../images/pro11.jpg" alt="" class="img-fluid"></a>
+</div>
+<div class="col-lg-3 col-md-4 col-sm-6">
+  <a href=""><img src="../images/pro12.jpg" alt="" class="img-fluid"></a>
+</div>
+  </div>
+
+
+  <div class="container p-4">
+  <div class="row justify-content-center">
+    <h1 class="mains-heading text-uppercase text-center fw-bold fs-1 mb-4">still need more?</h1>
+    <a href="" class=" arrival-link btn text-light fs-3 text-uppercase fw-light">view all collections</a>
+    <h1 class="main-heading text-center mb-4 mt-5 text-uppercase"><span>summer sales</span></h1>
+  </div>
+
+  <div class="row justify-content-center mb-3">
+  <div class="col-lg-4 col-md-4 col-sm-6">
+  <a href=""><img src="../images/pro13.jpeg" alt="" class="img-fluid"></a>
+</div>
+<div class="col-lg-4 col-md-4 col-sm-6">
+  <a href=""><img src="../images/pro14.jpg" alt="" class="img-fluid"></a>
+</div>
+<div class="col-lg-4 col-md-4 col-sm-6">
+  <a href=""><img src="../images/pro15.jpeg" alt="" class="img-fluid"></a>
+</div>
+
+  </div>
+  <div class="row justify-content-center">
+  <div class="col-lg-3 col-md-4 col-sm-6">
+  <a href=""><img src="../images/pro16.jpg" alt="" class="img-fluid"></a>
+</div>
+<div class="col-lg-3 col-md-4 col-sm-6">
+  <a href=""><img src="../images/pro17.jpg" alt="" class="img-fluid"></a>
+</div>
+<div class="col-lg-3 col-md-4 col-sm-6">
+  <a href=""><img src="../images/pro18.jpg" alt="" class="img-fluid"></a>
+</div>
+<div class="col-lg-3 col-md-4 col-sm-6">
+  <a href=""><img src="../images/pro19.jpg" alt="" class="img-fluid"></a>
+</div>
+
+  </div>
+ 
+</div>
+
+<div class="container p-4">
+<div class="accordion" id="accordionExample">
+
+
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingThree">
+      <button class="accordion-button collapsed fs-2 text-dark" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+      Philosophy
+      </button>
+    </h2>
+    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+      <div class="accordion-body fs-5">
+      No amount of clothes in a woman’s wardrobe is enough. Pakistani women are big fashion-freaks with great taste in Eastern wear. To fulfill your desire of looking best at every event, we have brought a huge variety of <strong>dresses for women</strong> with trending prints, latest designs and styles.
+    </div>
+  </div>
+</div>
+</div>
     </section>
     <!-- ================== Home Section End=============== -->
     <!-- ========== ionicons JS Links====================== -->
