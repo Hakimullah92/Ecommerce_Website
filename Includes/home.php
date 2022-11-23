ed81e84ec747863aa12118f044dc0d5ebcbd697f
Hakim Ullah <106232159+Hakimullah92@users.noreply.github.com>
2022-11-23 09:22:57 +0500

    Making banner in home.php as a carousel

---
+++ Includes/home.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+    <!-- ============== Bootstrap CSS File Link============= -->
+
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+
+    <!-- ================= Custom CSS File=================== -->
+
+    <link rel="stylesheet" href="../CSSFiles/home.css">
+</head>
+<body>
+    <!-- ================== Home Section Start=============== -->
+    <section id="home" class="home">
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <div class="home-banner home-banner-1"></div>
+    </div>
+    <div class="carousel-item">
+    <div class="home-banner home-banner-2"></div>
+    </div>
+   
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+    </section>
+    <!-- ================== Home Section End=============== -->
+    <!-- ========== ionicons JS Links====================== -->
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+   <!-- ===================== Bootstrap JS File Link=================== -->
+
+    <script src="../bootstrap/js/bootstrap.js"></script>
+
+    <!-- ================ Custom JS File================== -->
+
+    <script src="../JSFiles/header.js"></script>
+</body>
+</html>
