5522bf18faf2f56e28185ed5286638c5df1d768f
Hakim Ullah <106232159+Hakimullah92@users.noreply.github.com>
2022-11-24 17:19:21 +0500

    Making Responsive Footer

---
+++ Includes/footer.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FOOTER</title>
+    <!-- ============== Bootstrap CSS File Link============= -->
+
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+
+    <!-- ================= Custom CSS File=================== -->
+
+    <link rel="stylesheet" href="../CSSFiles/footer.css">
+</head>
+<body>
+    
+   <section class="footer" id="footer">
+    <div class="container-fluid">
+        <div class="row p-4 bg-dark justify-content-center align-items-center gap-5">
+         <div class="col-md-3 col-sm-6">
+            <h3 class=" follow-heading text-white fs-1 fw-light text-uppercase pb-3 mb-3">follow us</h3>
+            <div class="icons d-flex justify-content-start gap-5">
+                <a href=""><ion-icon name="logo-facebook" class="fs-1 text-white"></ion-icon></a>
+                <a href=""><ion-icon name="logo-instagram" class="fs-1 text-white"></ion-icon></a>
+                <a href=""><ion-icon name="logo-whatsapp" class="fs-1 text-white"></ion-icon></a>
+                <a href=""><ion-icon name="call-outline" class="fs-1 text-white"></ion-icon></a>
+                
+            </div>
+            <h4 class=" follow-heading text-white fs-1 fw-light text-uppercase pb-3  mt-3 mb-3">we accept</h4>
+            <div class="icons d-flex justify-content-start gap-5">
+                <a href=""><ion-icon name="logo-closed-captioning" class="fs-1 text-white"></ion-icon></a>
+               <a href=""> <ion-icon name="chatbox-ellipses-outline" class="fs-1 text-white"></ion-icon></a>
+                <a href=""><ion-icon name="home-outline" class="fs-1 text-white"></ion-icon></a>
+                <a href=""><ion-icon name="tablet-landscape-outline" class="fs-1 text-white"></ion-icon></a>
+                
+            </div>
+         </div>
+         <div class="col-md-4 col-sm-6">
+         <h3 class=" follow-heading text-white fs-1 fw-light text-uppercase pb-3 mb-3">follow us</h3>
+         <h3 class="text-white fs-1 fw-light text-upeercase">Address</h3>
+         <p class="text-white fs-4 fw-light mb-3">Brothers warehouse, 7.5km shekhupura Road, shahdara, lahore</p>
+         <h3 class="text-white fs-1 fw-light text-upeercase">Contact no</h3>
+         <p class="text-white fs-4 fw-light">+92 423 7380792</p>
+         <p class="text-white fs-4 fw-light">+92 346 4004443</p>
+
+         <h3 class="text-white fs-1 fw-light text-upeercase mt-3">Email</h3>
+         <p class="text-white fs-4 fw-light">+92 423 7380792</p>
+         </div>
+         <div class="col-md-3 col-sm-6 mt-5">
+            <h4 class=" follow-heading text-white fs-1 fw-light text-uppercase pb-3  mt-3 mb-3">quick links</h4>
+            <div class="links mt-3">
+                <div class="link mb-3"><a href="" class="text-white fs-2 text-uppercase  text-decoration-none">Home</a></div>
+                <div class="link mb-3"><a href="" class="text-white fs-2  text-uppercase text-decoration-none">cart</a></div>
+                <div class="link mb-3"><a href="" class="text-white fs-2 text-uppercase  text-decoration-none">my accounts</a></div>
+                <div class="link mb-3"><a href="" class="text-white fs-2 text-uppercase  text-decoration-none">shop</a></div>
+                
+            </div>
+         </div>
+
+        </div>
+    </div>
+   </section>
+
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+   <!-- ===================== Bootstrap JS File Link=================== -->
+
+    <script src="../bootstrap/js/bootstrap.js"></script>
+
+    <!-- ================ Custom JS File================== -->
+
+    <script src="../JSFiles/header.js"></script>
+</body>
+</html>
