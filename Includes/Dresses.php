78b80f160e9c739c5a88452933b6cae2b34fe654
Hakim Ullah <106232159+Hakimullah92@users.noreply.github.com>
2022-11-26 21:18:25 +0500

    Making Responsive Dresses WebPage

---
+++ Includes/Dresses.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VIEW ALL DRESSES</title>
+    <!-- ============== Bootstrap CSS File Link============= -->
+
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+
+    <!-- ================= Custom CSS File=================== -->
+
+    <link rel="stylesheet" href="../CSSFiles/dresses.css">
+</head>
+<body>
+   <section id="dresses" class="dresses">
+    <div class="container p-4">
+        <div class="row heading">
+            <h1 class="main-heading text-secondary text-center text-uppercase mb-4"><span>all products</span></h1>
+        </div>
+
+        <div class="row justify-content-center  text-md-start text-sm-center mb-4 text-center">
+            <div class="col-lg-4 col-md-6 col-sm-12 mb-sm-0 mb-3">
+                <div class="image">
+                    <a href=""><img src="../images/d1.webp" alt="" class="img-fluid" style="min-width:360px;"></a>
+                </div>
+                <div class="text-container">
+                    <h4 class="product-heading mt-1 mb-3 fs-3">Diyar-E-Dil DL-14 : 3Pc Embroidered Shirt With Velvet Palachi Shawl</h4>
+                    <h4 class="mb-1"><span class="text-secondary text-decoration-line-through">RS7,999.00 </span>    <sub class="text-dark">RS6,800.00</sub></h4>
+                    <a href="" class="btn btn-dark p-2 fs-3">Add to Cart</a>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12 mb-sm-0 mb-3">
+                <div class="image">
+                    <a href=""><img src="../images/d2.webp" alt="" class="img-fluid" style="min-width:360px;"></a>
+                </div>
+                <div class="text-container">
+                    <h4 class="product-heading mt-1 mb-3 fs-3">Diyar-E-Dil DL-14 : 3Pc Embroidered Shirt With Velvet Palachi Shawl</h4>
+                    <h4 class="mb-1"><span class="text-secondary text-decoration-line-through">RS7,999.00 </span>    <sub class="text-dark">RS6,800.00</sub></h4>
+                    <a href="" class="btn btn-dark p-2 fs-3">Add to Cart</a>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12 mb-sm-0 mb-3">
+                <div class="image">
+                    <a href=""><img src="../images/d3.webp" alt="" class="img-fluid" style="min-width:360px;"></a>
+                </div>
+                <div class="text-container">
+                    <h4 class="product-heading mt-1 mb-3 fs-3">Diyar-E-Dil DL-14 : 3Pc Embroidered Shirt With Velvet Palachi Shawl</h4>
+                    <h4 class="mb-1"><span class="text-secondary text-decoration-line-through">RS7,999.00 </span>    <sub class="text-dark">RS6,800.00</sub></h4>
+                    <a href="" class="btn btn-dark p-2 fs-3">Add to Cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center  text-md-start text-sm-center text-center mb-4">
+            <div class="col-lg-4 col-md-6 col-sm-12 mb-sm-0 mb-3">
+                <div class="image">
+                    <a href=""><img src="../images/d4.webp" alt="" class="img-fluid" style="min-width:360px;"></a>
+                </div>
+                <div class="text-container">
+                    <h4 class="product-heading mt-1 mb-3 fs-3">Diyar-E-Dil DL-14 : 3Pc Embroidered Shirt With Velvet Palachi Shawl</h4>
+                    <h4 class="mb-1"><span class="text-secondary text-decoration-line-through">RS7,999.00 </span>    <sub class="text-dark">RS6,800.00</sub></h4>
+                    <a href="" class="btn btn-dark p-2 fs-3">Add to Cart</a>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12 mb-sm-0 mb-3">
+                <div class="image">
+                    <a href=""><img src="../images/d5.webp" alt="" class="img-fluid" style="min-width:360px;"></a>
+                </div>
+                <div class="text-container">
+                    <h4 class="product-heading mt-1 mb-3 fs-3">Diyar-E-Dil DL-14 : 3Pc Embroidered Shirt With Velvet Palachi Shawl</h4>
+                    <h4 class="mb-1"><span class="text-secondary text-decoration-line-through">RS7,999.00 </span>    <sub class="text-dark">RS6,800.00</sub></h4>
+                    <a href="" class="btn btn-dark p-2 fs-3">Add to Cart</a>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12 mb-sm-0 mb-3">
+                <div class="image">
+                    <a href=""><img src="../images/d6.webp" alt="" class="img-fluid" style="min-width:360px;"></a>
+                </div>
+                <div class="text-container">
+                    <h4 class="product-heading mt-1 mb-3 fs-3">Diyar-E-Dil DL-14 : 3Pc Embroidered Shirt With Velvet Palachi Shawl</h4>
+                    <h4 class="mb-1"><span class="text-secondary text-decoration-line-through">RS7,999.00 </span>    <sub class="text-dark">RS6,800.00</sub></h4>
+                    <a href="" class="btn btn-dark p-2 fs-3">Add to Cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center  text-md-start text-sm-center text-center mb-4">
+            <div class="col-lg-4 col-md-6 col-sm-12 mb-sm-0 mb-3">
+                <div class="image">
+                    <a href=""><img src="../images/d7.webp" alt="" class="img-fluid" style="min-width:360px;"></a>
+                </div>
+                <div class="text-container">
+                    <h4 class="product-heading mt-1 mb-3 fs-3">Diyar-E-Dil DL-14 : 3Pc Embroidered Shirt With Velvet Palachi Shawl</h4>
+                    <h4 class="mb-1"><span class="text-secondary text-decoration-line-through">RS7,999.00 </span>    <sub class="text-dark">RS6,800.00</sub></h4>
+                    <a href="" class="btn btn-dark p-2 fs-3">Add to Cart</a>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12 mb-sm-0 mb-3">
+                <div class="image">
+                    <a href=""><img src="../images/d8.webp" alt="" class="img-fluid" style="min-width:360px;"></a>
+                </div>
+                <div class="text-container">
+                    <h4 class="product-heading mt-1 mb-3 fs-3">Diyar-E-Dil DL-14 : 3Pc Embroidered Shirt With Velvet Palachi Shawl</h4>
+                    <h4 class="mb-1"><span class="text-secondary text-decoration-line-through">RS7,999.00 </span>    <sub class="text-dark">RS6,800.00</sub></h4>
+                    <a href="" class="btn btn-dark p-2 fs-3">Add to Cart</a>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12 mb-sm-0 mb-3">
+                <div class="image">
+                    <a href=""><img src="../images/d9.webp" alt="" class="img-fluid" style="min-width:360px;"></a>
+                </div>
+                <div class="text-container">
+                    <h4 class="product-heading mt-1 mb-3 fs-3">Diyar-E-Dil DL-14 : 3Pc Embroidered Shirt With Velvet Palachi Shawl</h4>
+                    <h4 class="mb-1"><span class="text-secondary text-decoration-line-through">RS7,999.00 </span>    <sub class="text-dark">RS6,800.00</sub></h4>
+                    <a href="" class="btn btn-dark p-2 fs-3">Add to Cart</a>
+                </div>
+            </div>
+        </div>
+    </div>
+   </section>
+
+
+
+       <!-- ========== ionicons JS Links====================== -->
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+   <!-- ===================== Bootstrap JS File Link=================== -->
+
+    <script src="../bootstrap/js/bootstrap.js"></script> 
+</body>
+</html>
