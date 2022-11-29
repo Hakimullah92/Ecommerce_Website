0bba3f19caf47825e8f9507121d27d1d9d24f31d
Hakim Ullah <106232159+Hakimullah92@users.noreply.github.com>
2022-11-29 19:42:21 +0500

    Making Responsive trackorder page

---
+++ Includes/trackorder.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Track Your Order</title>
+    <!-- ============== Bootstrap CSS File Link============= -->
+
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+
+    <!-- ================= Custom CSS File=================== -->
+
+    <link rel="stylesheet" href="../CSSFiles/trackorder.css">
+</head>
+<body>
+<section class="tract-order" id="track-order">
+    <div class="container">
+        <div class="row heading-container">
+            <h1 class="main-heading text-uppercase text-secondary text-center fs-1 mb-5"><span>Tract your order</span></h1>
+            <div class="col-12 fs-2 text-center mb-5">To track your order please enter the following details and press the "Track" <br> button.</div>
+        </div>
+
+       <div class="row">
+        <form>
+  <div class="row mb-3 justify-content-center">
+    <div class="col-md-6 col-sm-12 p-2">
+        <label for="" class="mb-2 fs-3">Old Number</label>
+      <input type="number" class="form-control p-3" id="inputEmail3">
+    </div>
+  </div>
+  <div class="row mb-3 justify-content-center">
+    <div class=" col-md-6 col-sm-12 p-2">
+        <label for="" class="mb-2 fs-3" >Mobile Number</label>
+      <input type="number" class="form-control p-3" id="inputPassword3">
+    </div>
+  </div>
+  <div class="row mb-3 justify-content-center">
+    <div class=" col-md-6 col-sm-12 p-2">
+      <button class="track-btn fs-2 w-100 text-white p-2">Track</button>
+    </div>
+  </div>
+  
+</form>
+</div>
+    </div>
+</section>
+
+
+
+
+    <!-- =============== Ionicons JS Links==================== -->
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+   <!-- ===================== Bootstrap JS File Link=================== -->
+
+    <script src="../bootstrap/js/bootstrap.js"></script>
+</body>
+</html>
