2242f246cdf4a29b70c8d7a42b509fb4e9cbc373
Hakim Ullah <106232159+Hakimullah92@users.noreply.github.com>
2022-12-02 15:11:20 +0500

    Making Responsive mehr_o_maah.php

---
+++ Includes/mehr_o_maah.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mehr O Maah -kleren</title>
+    <!-- ============== Bootstrap CSS File Link============= -->
+
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+
+    <!-- ================= Custom CSS File=================== -->
+
+    <link rel="stylesheet" href="../CSSFiles/mehr_o_maah.css">
+</head>
+<body>
+    <section class="mehr" id="mehr">
+    <div class="container p-4">
+        <div class="links mb-3">
+            <a href="" class="text-dark text-decoration-none fs-4 text-uppercase">Home /</a>
+            <a href="" class="text-dark text-decoration-none fs-4 text-uppercase">Winter Collections /</a>
+            <a href="" class="text-dark text-decoration-none fs-4 text-uppercase">karandi /</a>
+            <a href="" class="text-dark text-decoration-none fs-4 text-uppercase">mehr o maah</a>
+        </div>
+        <div class="row section-heading mb-3">
+            <h1 class="main-heading text-center  fw-light text-dark text-uppercase mt-5 mb-5"><span>mehr_o_maah</span></h1>
+            <p class="para text-uppercase text-center fs-4 fw-light">Premium Unstitched Embroidered Slub Karandi 3pc Suites Collection</p>
+        </div>
+
+
+        <div class="row mb-4">
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/m1.webp" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">mehr_o_maah m-01: embroidered slub karandi</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/m2.webp" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">mehr_o_maah-04: embroidered slub karandi</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/m3.webp" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">mehr_o_maah-05: embroidered slub karandi</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+        </div>
+
+        <div class="row mb-4 mt-5">
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/m4.webp" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">mehr_o_maah-01: embroidered slub karandi</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/m5.webp" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">mehr_o_maah-02: embroidered slub karandi</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/m6.webp" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">mehr_o_maah-03: embroidered slub karandi</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+        </div>
+
+        <div class="row mb-4">
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/m7.webp" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">mehr_o_maah m-04: embroidered slub karandi</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">read more...</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/m8.webp" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">mehr_o_maah m-05: embroidered slub karandi</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">read more...</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/m9.webp" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">mehr_o_maah m-05: embroidered slub karandi</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">read more...</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/m10.webp" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">mehr_o_maah m-05: embroidered slub karandi</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">read more...</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/m11.webp" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">mehr_o_maah m-05: embroidered slub karandi</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">read more...</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/m12.webp" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">mehr_o_maah m-05: embroidered slub karandi</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">read more...</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/m13.webp" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">mehr_o_maah m-05: embroidered slub karandi</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">read more...</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/m14.webp" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">mehr_o_maah m-05: embroidered slub karandi</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">read more...</a>
+    </div>
+   </div>
+ 
+        </div>
+        <div class="row mt-3 mb-3">
+            <h1 class="text-uppercase text-center mb-2" style="color:rgb(5, 5, 53); font-size:3rem;">looking for more?</h1>
+            <a href="" class="text-white text-uppercase fs-2 fw-light text-center  p-2 m-auto text-decoration-none" style="background:rgb(5, 5, 53); width:300px;">view all collections</a>
+        </div>
+
+    </div>
+    </section>
+       <!-- =============== Ionicons JS Links==================== -->
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+   <!-- ===================== Bootstrap JS File Link=================== -->
+
+    <script src="../bootstrap/js/bootstrap.js"></script>
+</body>
+</html>
