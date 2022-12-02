a377c7af0eae183fdaf29f19b8e3a7df4c139952
Hakim Ullah <106232159+Hakimullah92@users.noreply.github.com>
2022-12-02 14:49:06 +0500

    Making Responsive diyar_e_dil.php

---
+++ Includes/diyar_e_dil.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shafaq-Kleren</title>
+    <!-- ============== Bootstrap CSS File Link============= -->
+
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+
+    <!-- ================= Custom CSS File=================== -->
+
+    <link rel="stylesheet" href="../CSSFiles/diyar_e_dil.css">
+</head>
+<body>
+    <section class="diyar" id="diyar">
+    <div class="container p-4">
+        <div class="links mb-3">
+            <a href="" class="text-dark text-decoration-none fs-4 text-uppercase">Home /</a>
+            <a href="" class="text-dark text-decoration-none fs-4 text-uppercase">Winter Collections /</a>
+            <a href="" class="text-dark text-decoration-none fs-4 text-uppercase">linen /</a>
+            <a href="" class="text-dark text-decoration-none fs-4 text-uppercase">diyar_e_dil</a>
+        </div>
+        <div class="row section-heading mb-3">
+            <h1 class="main-heading text-center  fw-light text-dark text-uppercase mt-5 mb-5"><span>diyar_e_dil</span></h1>
+            <p class="para text-uppercase text-center fs-4 fw-light">Premium Unstitched 3pc Embroidered Shirt with Wool / iyar_e_dil dllach</p>
+        </div>
+
+
+        <div class="row mb-4">
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/dl1.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">diyar_e_dil dl-01: embroidered dhanak</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/dl2.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">diyar_e_dil dl-04: embroidered dhanak</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/dl3.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">diyar_e_dil dl-05: embroidered dhanak</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+        </div>
+
+        <div class="row mb-4 mt-5">
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/dl4.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">diyar_e_dil dl-01: embroidered dhanak</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/dl5.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">diyar_e_dil dl-02: embroidered dhanak</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/dl6.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">diyar_e_dil dl-03: embroidered dhanak</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+        </div>
+
+        <div class="row mb-4">
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/dl7.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">diyar_e_dil dl-04: 3pc embroidered shirt withvelvet palachishawl</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/dl8.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">diyar_e_dil dl-05: 3pc embroidered shirt withvelvet palachishawl</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/dl9.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">diyar_e_dil dl-05: 3pc embroidered shirt withvelvet palachishawl</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/dl10.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">diyar_e_dil dl-05: 3pc embroidered shirt withvelvet palachishawl</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/dl11.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">diyar_e_dil dl-05: 3pc embroidered shirt withvelvet palachishawl</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/dl12.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">diyar_e_dil dl-05: 3pc embroidered shirt withvelvet palachishawl</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/dl13.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">diyar_e_dil dl-05: 3pc embroidered shirt withvelvet palachishawl</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/dl14.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">diyar_e_dil dl-05: 3pc embroidered shirt withvelvet palachishawl</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+  
+ 
+        </div>
+        <div class="row mt-3 mb-3">
+            <h1 class="text-uppercase text-center mb-2" style="color:rgb(5, 5, 53); font-size:3rem;">looking for more?</h1>
+            <a href="" class="text-white text-uppercase fs-2 fw-light text-center  p-2 m-auto text-decoration-none" style="background:rgb(5, 5, 53); width:300px;">view all collections</a>
+        </div>
+
+    </div>
+    </section>
+       <!-- =============== Ionicons JS Links==================== -->
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+   <!-- ===================== Bootstrap JS File Link=================== -->
+
+    <script src="../bootstrap/js/bootstrap.js"></script>
+</body>
+</html>
