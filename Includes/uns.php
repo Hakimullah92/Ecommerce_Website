b6ebccc4a64ed6029b5b14c89ac94c8109e883ef
Hakim Ullah <106232159+Hakimullah92@users.noreply.github.com>
2022-12-03 15:26:28 +0500

    Making Responsive uns.php

---
+++ Includes/uns.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UNS -kleren</title>
+    <!-- ============== Bootstrap CSS File Link============= -->
+
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+
+    <!-- ================= Custom CSS File=================== -->
+
+    <link rel="stylesheet" href="../CSSFiles/uns.css">
+</head>
+<body>
+    <section class="uns" id="uns">
+    <div class="container p-4">
+        <div class="links mb-3">
+            <a href="" class="text-dark text-decoration-none fs-4 text-uppercase">Home /</a>
+            <a href="" class="text-dark text-decoration-none fs-4 text-uppercase">summer Collections /</a>
+            <a href="" class="text-dark text-decoration-none fs-4 text-uppercase">lawn /</a>
+            <a href="" class="text-dark text-decoration-none fs-4 text-uppercase">embroidered lawn collection /</a>
+            <a href="" class="text-dark text-decoration-none fs-4 text-uppercase">uns
+            </a>
+        </div>
+        <div class="row section-heading mb-3">
+            <h1 class="main-heading text-center  fw-light text-dark text-uppercase mt-5 mb-5"><span>uns</span></h1>
+            <p class="para text-uppercase text-center fs-4 fw-light">3pc Luxury Embroidered & Printed Lawn shirt with Chiffon Embroidered Dupatta & Plain Lawn Trouser.</p>
+        </div>
+
+
+        <div class="row mb-4">
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/u1.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">uns d-01: unstitched embroidered & lawn 3pc</h3>
+        <p class="text-uppercase fs-3 text-dark mb-4"><span class="">rs 7600.00  </span></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/u2.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">uns d-02: unstitched embroidered & lawn 3pc</h3>
+        <p class="text-uppercase fs-3 text-dark mb-4"><span class="">rs 5600.00  </span></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/u3.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">uns d-03: unstitched embroidered & lawn 3pc</h3>
+        <p class="text-uppercase fs-3 text-dark mb-4"><span class="">rs 5600.00  </span></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+        </div>
+
+        <div class="row mb-4 mt-5">
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/u4.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">uns d-04: unstitched embroidered & lawn 3pc</h3>
+        <p class="text-uppercase fs-3 text-dark mb-4"><span class="">rs 5600.00  </span></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">read more...</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/u5.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">uns d-05: unstitched embroidered & lawn 3pc</h3>
+        <p class="text-uppercase fs-3 text-dark mb-4"><span class="">rs 5600.00  </span></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">read more...</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/u6.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">uns d-06: unstitched embroidered & lawn 3pc</h3>
+        <p class="text-uppercase fs-3 text-dark mb-4"><span class="">rs 5600.00  </span></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">read more...</a>
+    </div>
+   </div>
+        </div>
+
+        <div class="row mb-4">
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/u7.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">uns d-07: unstitched embroidered & lawn 3pc</h3>
+        <p class="text-uppercase fs-3 text-dark mb-4"><span class="">rs 5600.00  </span></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">read more...</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/u8.jpg" altuns dass="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">uns d-08: unstitched embroidered & lawn 3pc</h3>
+        <p class="text-uppercase fs-3 text-dark mb-4"><span class="">rs 5600.00  </span></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">read more...</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/u9.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">uns d-09: unstitched embroidered & lawn 3pc</h3>
+        <p class="text-uppercase fs-3 text-dark mb-4"><span class="">rs 5600.00  </span></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">read more...</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/u10.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">uns d-10: unstitched embroidered & lawn 3pc</h3>
+        <p class="text-uppercase fs-3 text-dark mb-4"><span class="">rs 5600.00  </span></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">read more....</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/u11.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">uns d-11: unstitched embroidered & lawn 3pc</h3>
+        <p class="text-uppercase fs-3 text-dark mb-4"><span class="">rs 5600.00  </span></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">read more...</a>
+    </div>
+   </div>
+ 
+  
+ 
+        </div>
+        <div class="row mt-3 mb-3">
+            <h1 class="text-uppercase text-center mb-2" style="color:rgb(5, 5, 53); font-size:3rem;">looking for more?</h1>
+            <a href="" class="text-white text-uppercase fs-2 fw-light text-center  p-2 m-auto text-decoration-none" style="background:rgb(5, 5, 53); width:300px;">view all collections</a>
+        </div>
+
+    </div>
+    </section>
+       <!-- =============== Ionicons JS Links==================== -->
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+   <!-- ===================== Bootstrap JS File Link=================== -->
+
+    <script src="../bootstrap/js/bootstrap.js"></script>
+</body>
+</html>
