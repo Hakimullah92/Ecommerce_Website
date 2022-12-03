6b71f13ebab29657e31bcc809340e26d0b2022c2
Hakim Ullah <106232159+Hakimullah92@users.noreply.github.com>
2022-12-03 14:13:33 +0500

    Making Responsive Ibtida.php

---
+++ Includes/ibtida.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ibtida -kleren</title>
+    <!-- ============== Bootstrap CSS File Link============= -->
+
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+
+    <!-- ================= Custom CSS File=================== -->
+
+    <link rel="stylesheet" href="../CSSFiles/ibtida.css">
+</head>
+<body>
+    <section class="ibtida" id="ibtida">
+    <div class="container p-4">
+        <div class="links mb-3">
+            <a href="" class="text-dark text-decoration-none fs-4 text-uppercase">Home /</a>
+            <a href="" class="text-dark text-decoration-none fs-4 text-uppercase">summer Collections /</a>
+            <a href="" class="text-dark text-decoration-none fs-4 text-uppercase">lawn /</a>
+            <a href="" class="text-dark text-decoration-none fs-4 text-uppercase">embroidered lawn collection /</a>
+            <a href="" class="text-dark text-decoration-none fs-4 text-uppercase">ibtida
+            </a>
+        </div>
+        <div class="row section-heading mb-3">
+            <h1 class="main-heading text-center  fw-light text-dark text-uppercase mt-5 mb-5"><span>ibtida</span></h1>
+            <p class="para text-uppercase text-center fs-4 fw-light">3pc Luxury Embroidered Lawn shirt with Chiffon Embroidered Dupatta & Plain Lawn Trouser.</p>
+        </div>
+
+
+        <div class="row mb-4">
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/i1.webp" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">ibtida i-01: embroidered lawn</h3>
+        <div class="stars d-flex gap-2 mb-2">
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            
+        </div>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/i2.webp" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">ibtida i-02: embroidered lawn</h3>
+        <div class="stars d-flex gap-2 mb-2">
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            
+        </div>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/i3.webp" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">ibtida i-03: embroidered lawn</h3>
+        <div class="stars d-flex gap-2 mb-2">
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            
+        </div>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+        </div>
+
+        <div class="row mb-4 mt-5">
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/i4.webp" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">ibtida i-04: embroidered lawn</h3>
+        <div class="stars d-flex gap-2 mb-2">
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            
+        </div>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/i5.webp" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">dastak d-05:embroidered lawn</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/i6.webp" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">ibtida i-06: embroidered lawn</h3>
+        <div class="stars d-flex gap-2 mb-2">
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            
+        </div>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+        </div>
+
+        <div class="row mb-4">
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/i7.webp" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">ibtida i-07: embroidered lawn</h3>
+        <div class="stars d-flex gap-2 mb-2">
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            
+        </div>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/i8.webp" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">ibtida i-08: embroidered lawn</h3>
+        <div class="stars d-flex gap-2 mb-2">
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            
+        </div>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/i9.webp" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">ibtida i-09: embroidered lawn</h3>
+        <div class="stars d-flex gap-2 mb-2">
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            
+        </div>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/i10.webp" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">ibtida i-10: embroidered lawn</h3>
+        <div class="stars d-flex gap-2 mb-2">
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            
+        </div>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">read more...</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/i11.webp" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">ibtida i-11: embroidered lawn</h3>
+        <div class="stars d-flex gap-2 mb-2">
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            <ion-icon name="star-outline" class="star fs-3"></ion-icon>
+            
+        </div>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 7600.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS6,640.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+ 
+  
+ 
+        </div>
+        <div class="row mt-3 mb-3">
+            <h1 class="text-uppercase text-center mb-2" style="color:rgb(5, 5, 53); font-size:3rem;">looking for more?</h1>
+            <a href="" class="text-white text-uppercase fs-2 fw-light text-center  p-2 m-auto text-decoration-none" style="background:rgb(5, 5, 53); width:300px;">view all collections</a>
+        </div>
+
+    </div>
+    </section>
+       <!-- =============== Ionicons JS Links==================== -->
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+   <!-- ===================== Bootstrap JS File Link=================== -->
+
+    <script src="../bootstrap/js/bootstrap.js"></script>
+</body>
+</html>
