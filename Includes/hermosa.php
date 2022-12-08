3c0d3f677f566c442be5e5c1dc02b941b51ce102
Hakim Ullah <106232159+Hakimullah92@users.noreply.github.com>
2022-12-08 10:07:31 +0500

    Making Responsive hermosa.php

---
+++ Includes/hermosa.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hermosa -kleren</title>
+    <!-- ============== Bootstrap CSS File Link============= -->
+
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+
+    <!-- ================= Custom CSS File=================== -->
+
+    <link rel="stylesheet" href="../CSSFiles/hermosa.css">
+</head>
+<body>
+    <section class="hermosa" id="hermosa">
+    <div class="container p-4">
+        <div class="links mb-3">
+            <a href="" class="text-dark text-decoration-none fs-4 text-uppercase">Home /</a>
+            <a href="" class="text-dark text-decoration-none fs-4 text-uppercase">Winter Collections /</a>
+            <a href="" class="text-dark text-decoration-none fs-4 text-uppercase">linen /</a>
+            <a href="" class="text-dark text-decoration-none fs-4 text-uppercase">hermosa
+            </a>
+        </div>
+        <div class="row section-heading mb-3">
+            <h1 class="main-heading text-center  fw-light text-dark text-uppercase mt-5 mb-5"><span>hermosa</span></h1>
+            <p class="para text-uppercase text-center fs-4 fw-light">Premium Unstitched, Digital printed & Embroidered Linen Collection</p>
+        </div>
+
+
+        <div class="row mb-4">
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/h1.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">hermosa h-01 </h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 5700.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS4,845.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/im2.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">hermosa h-02</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 5700.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS4,845.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">read more...</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/h3.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">hermosa h-03</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 5700.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS4,845.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">read more...</a>
+    </div>
+   </div>
+        </div>
+        <div class="row mb-4">
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/h4.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2"hermosa h--0d</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs57900.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> R4,8450.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">add to cart</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/h5.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">hermosa h-02</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 5700.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS4,845.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">read more...</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/h6.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">hermosa h-03</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 5700.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS4,845.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">read more...</a>
+    </div>
+   </div>
+        </div>
+
+        <div class="row mb-4">
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/h7.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">hermosa h-07</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 5700.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS4,845.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">read more...</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/h8.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">hermosa h-08</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 5700.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS4,845.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">read more...</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/im9.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">hermosa h-09</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 5700.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS4,845.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">read more...</a>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 mb-4">
+    <div class="image">
+        <a href=""><img src="../images/h10.jpg" alt="pic"class="img-fluid w-100"></a>
+    </div>
+    <div class="text">
+        <h3 class="text-uppercase fs-4 mt-2 mb-2">hermosa h-10</h3>
+        <p class="text-uppercase fs-3 text-secondary mb-4"><span class="text-decoration-line-through">rs 5700.00  </span><sub class="text-dark text-decoration-underline fs-3 fw-bold"> RS4,845.00</sub></p>
+        <a href="" class="cart text-white text-decoration-none text-capitalize fs-3 fw-light">read more...</a>
+    </div>
+   </div>
+   </div>
+ 
+  
+ 
+        </div>
+        <div class="row mt-3 mb-3">
+            <h1 class="text-uppercase text-center mb-2" style="color:rgb(5, 5, 53); font-size:3rem;">looking for more?</h1>
+            <a href="" class="text-white text-uppercase fs-2 fw-light text-center  p-2 m-auto text-decoration-none" style="background:rgb(5, 5, 53); width:300px;">view all collections</a>
+        </div>
+
+    </div>
+    </section>
+       <!-- =============== Ionicons JS Links==================== -->
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+   <!-- ===================== Bootstrap JS File Link=================== -->
+
+    <script src="../bootstrap/js/bootstrap.js"></script>
+</body>
+</html>
